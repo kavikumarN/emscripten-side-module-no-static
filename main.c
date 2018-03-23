c80b95121f7ccb3e4ae66c36761539709466587e
Michael Droettboom <mdboom@gmail.com>
2018-03-23 11:35:35 -0400

    Demonstrate zero'd static data in SIDE_MODULE

--- main.c
+++ main.c
@@ -4,8 +4,17 @@
 int main(int argc, char *argv[])
 {
   void *mod1 = dlopen("mod1.so", RTLD_NOW);
-  void *(*ptr2)(int) = dlsym(mod1, "mod1func");
-  ptr2(44);
+  int (*ptr2)(int) = dlsym(mod1, "mod1func");
+  int x = ptr2(44);
+
+  if (x)
+    {
+      printf("No static string\n");
+    } else
+    {
+      printf("Found static string\n");
+
+    }
 
   return 0;
 }
--- mod1.c
+++ mod1.c
@@ -2,7 +2,9 @@
 #include <math.h>
 
 
-void mod1func(int x)
+int mod1func(int x)
 {
-  printf("mod1func %f\n", pow(2, x));
+  const char* msg = "Message";
+
+  return (msg[0] == 0);
 }
